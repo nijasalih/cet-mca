d25da1b06d5fae4d024065fcbbb2d5900b6757b9
nijasalih <nijasalih@gmail.com>
2023-11-30 15:07:57 +0530

    ads-doubly-link-working-code

--- mca.ds/doublylinked.c
+++ mca.ds/doublylinked.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 struct Node{
     int data;
@@ -15,11 +16,102 @@ struct Node* create_node(int data){
     return new_node;
 }
 
+void display_asc(){
+    if(head==NULL){
+        printf("\nLinked List is Empty!");
+    }
+    else{
+        struct Node *temp = head;
+        printf("Linked List in ascending order : ");
+        while(temp!=NULL){
+            printf("%d ",temp->data);
+            temp=temp->next;
+        }
+        printf("\n");
+    }
+}
+
+void display_desc(){
+    if(head==NULL){
+        printf("\nLinked List is Empty!");
+    }
+    else{
+        struct Node *temp = tail;
+        printf("Linked List in ascending order : ");
+        while(temp!=NULL){
+            printf("%d ",temp->data);
+            temp=temp->prev;
+        }
+        printf("\n");
+    }
+}
+
+void insert_end(int data){
+    struct Node* new_node = create_node(data);
+
+    if(head==NULL){
+        head = tail = new_node;
+    }
+    else{
+        new_node->prev = tail;
+        tail->next = new_node; 
+        tail = new_node;
+    }
+    display_asc();
+    display_desc();
+    printf("\n");
+}
+void insert_at(int data){
+    int pos,i=2;
+    printf("Enter the position to be inserted : ");
+    scanf("%d",&pos);
+
+    if(head==NULL){
+        
+    }
+    else{
+
+    }
+
+}
+
+void delete_end(){
+    if(head==NULL){
+        printf("\nLinked List is Empty!");
+    }
+    else{
+        if(tail->prev == NULL){
+            free(tail);
+            tail = head = NULL;
+        }
+        else{
+            tail=tail->prev;
+            free(tail->next);
+            tail->next = NULL;
+            
+            display_asc();
+            display_desc();
+        }
+    }
+}
+
+
 int main(){
-    
+    insert_end(1);
+    insert_end(2);
+    insert_end(3);
+    insert_end(4);
+    insert_end(5);
+
+    delete_end();
+    delete_end();
+    delete_end();
+    delete_end();
+    delete_end();
+    delete_end();
+
+
     
     return 0;
 }
-// insert_beg(){
 
-// }
